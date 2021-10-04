628b2eed318664607e4a10a9cd1394889e933285
milameitriani21 <milameitriani21@gmail.com>
2021-10-04 21:20:11 +0800

    Tugas Web

---
+++ frontpage/beranda.php
@@ -0,0 +1,62 @@
+<div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+			<h1 class="my-4 text-3xl md:text-5xl text-black-100 font-bold leading-tight text-center md:text-left slide-in-bottom-h1 text-indigo-50">Bibit Ikan Kerapu</h1>
+			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle text-indigo-50">Menyediakan berbagai macam jenis bibit ikan kerapu</p>
+		</div>
+		
+		<!--Right Col-->
+		<div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
+            <img class="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="asssets/K1.jpg"> 
+		</div>
+
+        <div class="grid grid-cols-2 gap-2">
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+				<div class="md:flex-shrink-0">
+					<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/cantang 1.jpg" alt="Man looking at item at a store">
+				</div>
+				<div class="p-8">
+					<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kerapu Cantang</div>
+					<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+					<p class="mt-2 text-gray-500">Kerapu Cantang merupakan jenis ikan kerapu yang banyak digemari oleh pecinta ikan</p>
+				</div>
+				</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/macan 1.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kerapu Macan</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">kerapu macan merupakan salah satu komoditas laut yang memiliki permintaan yang banyak dipasaran setiap tahunnya baik nasional maupun internasional. </p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/cantik 1.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kerapu Cantik</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Kerapu cantik merupakan Ikan kerapu hibrida hasil persilangan antara ikan kerapu macan (Epinephelus fuscoguttatus) betina dengan ikan kerapu batik (Epinephelus polyphekadion) jantan. </p>
+					</div>
+					</div>
+			</div>
+			<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl slide-in-bottom">
+				<div class="md:flex grid grid-cols-2">
+					<div class="md:flex-shrink-0">
+						<img class="h-48 w-full object-cover md:h-full md:w-48" src="asssets/kerang 2.jpg">
+					</div>
+					<div class="p-8">
+						<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Kerapu kerang</div>
+						<a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"><button class="bg-purple-400 p-1 rounded-lg shadow-md">Pemesanan</button></a>
+						<p class="mt-2 text-gray-500">Ikan kerapu kerang sering disebut dengan giant grouper. Nama tersebut diberikan karena ukuran dari ikan kerapu jenis ini yang di atas rata-rata. Karakteristik dari ikan ini adalah tubuh berwarna kuning serta terdapat bulatan hitam pada setiap siripnya.</p>
+					</div>
+					</div>
+			</div>
+		</div>
+
+		</div>
---
+++ frontpage/rekap.php
@@ -0,0 +1,40 @@
+<div class="items-center bg-white rounded-xl shadow-md overflow-hidden md:max-w-full slide-in-bottom">
+        <div class="py-12">
+          <h2 class="text-2xl font-bold">Pemesanan</h2>
+            <div class="mt-8 max-w-md">
+            <div class="grid grid-cols-1 gap-6">
+              <label class="block">
+                <span class="text-gray-700">Nama Lengkap</span>
+                <input type="text" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" placeholder=""/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">No HP / Alamat</span>
+                <input type="number" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" placeholder=""/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Jumlah</span>
+                <input type="text" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Tanggal memesan</span>
+                <input type="Date" class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Jenis Bibit Kerapu</span>
+                <select class="block w-full mt-1  rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0">
+                  <option>Bibit kerapu cantang</option>
+                  <option>Bibit kerapu macan</option>
+                  <option>Bibit kerapu cantik</option>
+                  <option>Bibit kerapu kerang</option>
+                </select>
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Keterangan pada penjual</span>
+                <textarea
+                  class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0" rows="3"
+                ></textarea>
+              </label>
+            </div>
+            </div>
+        </div>
+</div>
